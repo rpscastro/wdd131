d73a6b5817ccd850fb9261d97b38ace8bd18ce31
rpscastro <rafael.p.s.castro@gmail.com>
2024-05-07 16:15:31 -0300

    Web Design Principles

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,26 +1,29 @@
 const button = document.querySelector('button');
 const input = document.querySelector('#favchap');
 const list = document.querySelector('ul');
-const deleteButton = document.createElement('button');
-const li = document.createElement('li');
+
+
 
 
 button.addEventListener('click', function () {
     // Code to execute when the element is clicked
     if (input.value.trim() !== '') {
 
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
         li.textContent = input.value;
         deleteButton.textContent = '❌';
         li.append(deleteButton);
-        list.appendChild(li);
+        list.append(li);
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        })
+        
+        input.value = '';
+        input.focus();
     }
     else input.focus();
 });
 
-deleteButton.addEventListener('click', function () {
-    list.removeChild(li);
-    input.focus();
-})
-
-input.value = '';
-input.focus();
