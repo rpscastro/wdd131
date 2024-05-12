09b316fcaa9d7615489d5c149d2726ee94c8d5f0
rpscastro <rafael.p.s.castro@gmail.com>
2024-05-11 22:57:59 -0300

    Audit 3

--- scripts/place.js
+++ scripts/place.js
@@ -1,43 +1,30 @@
 // select the DOM elements for output
 const temp = 10;
-const wind = 5; 
+const wind = 5;
 const temperature = document.querySelector("#temperature");
 const windSpeedy = document.querySelector("#wind");
 const windChill = document.querySelector("#chill");
- 
+
 temperature.innerHTML = `${temp} °C`;
 windSpeedy.innerHTML = `${wind} km/h`;
 
 //Converts Celsius to Fahrenheit
-function temperatureFahrenheit (temperatureCelsius) {
-
-    return (9 / 5 * temperatureCelsius) + 32;
-}
+let celsiusToFahrenheit = (celsius) => (9 / 5 * celsius) + 32;
 
 //Converts Fahrenheit to Celsius 
-function temperatureCelsius (temperatureFahrenheit) {
-
-    return (temperatureFahrenheit - 32) * 5 / 9;
-}
+let fahrenheitToCelsius = (fahrenheit) => (fahrenheit - 32) * 5 / 9;
 
 //Converts Km/h to mp/h
-function mph (kmph) {
-
-    return kmph/1.609344;
-}
+let kmphToMph = (kmph) => kmph / 1.609344;
 
 //Calculate WindChill
-function calculateWindChill (t, w){
-    
-    return temperatureCelsius(35.74 + 0.6215* temperatureFahrenheit(t) - 35.75 * Math.pow(mph(w),0.16) + 0.4275 * temperatureFahrenheit(t) * Math.pow(mph(w),0.16));
-}
+let calculateWindChill = (t, w) => 35.74 + 0.6215 * t - 35.75 * Math.pow(w, 0.16) + 0.4275 * t * Math.pow(w, 0.16);
 
 
-
-
+//If the conditions are not met, then display "N/A", which means not applicable
 if (temp <= 10 && wind > 4.8) {
 
-    windChill.innerHTML = `${calculateWindChill(temp, wind).toFixed(1)} °C`;
+    windChill.innerHTML = `${fahrenheitToCelsius(calculateWindChill(celsiusToFahrenheit(temp), kmphToMph(wind))).toFixed(1)} °C`;
 
 }
 else {
